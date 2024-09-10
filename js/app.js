f92bcd401558a0fb816e1d00fd1dc2bd923de6b8
andres-lentijo <116856708+andres-lentijo@users.noreply.github.com>
2024-09-10 01:56:55 -0500

    javascript

---
+++ js/app.js
@@ -0,0 +1,43 @@
+// Comentario de una linea
+/*
+* Comentario multilinea
+*/
+
+console.log("Hola Mundo");
+
+// Variables
+let numero1 = 45;
+let numero2,
+let suma;
+
+
+const pi = 3.14156;
+let estado = true;
+let numero3 = 78;
+let direccion = "cra 140 7a 40";
+
+numero3 = 125;
+console.log(numero1); 
+console.log(numero2); 
+console.log(pi); 
+console.log(estado); 
+console.log(numero3);
+
+Suma = numero1 + numero2;
+
+console.log(suma)
+
+
+
+// Concatenar valores
+
+    console.log('EL PRIMER VALOR ES: ' + numero1)
+    console.log('EL SEGUNDO VALOR ES: ' + numero2)
+    console.log('LA SUMA DE ' + numero1 + 'Y' + numero2 + ' ES: ' + suma );
+    console.log('LA SUMA DE ' + numero1 + 'Y' + numero2 + ' ES: ' + suma );
+
+
+    // Literal Template
+    console.log('LA SUMA DE ${numero1} Y ${numero2} ES: ${suma}');
+    console.log('LA RESTA DE ${numero1} Y ${numero2) ES: ${resta}');
+    console.log('LA MULTIPLICACION DE ${numero1} Y ${numero2} ES: ${multiplicacion}');  
